7f6c75f291a8e3416208ab4e23a2982cedd4e867
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 22:46:27 +0100

    Removed unused shoppapp? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :shopapp?
-
   def render_404
     render(:file => "#{Rails.root}/public/404.html", :layout => false, :status => 404)
-  end
-
-  def shopapp?
-    request.domain =~ /(shopapp|shop-app\.herokuapp)\.\w{2,3}/ 
   end
 end
