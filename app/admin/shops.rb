1468343776543f76079e343a40b6489110a704fc
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 23:08:07 +0100

    Made hostname clickable

--- app/admin/shops.rb
+++ app/admin/shops.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register Shop do
     index do
         column :name
-        column :hostname
+        column :hostname, :sortable => :hostname do |shop|
+            link_to shop.hostname, "http://#{shop.hostname}"
+        end
         default_actions
     end
 end
