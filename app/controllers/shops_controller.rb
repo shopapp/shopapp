03d636024ac031f33ec38a0520afec8ea4e43525
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 22:52:43 +0100

    Removed shop#index action

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,8 +1,4 @@
 class ShopsController < ApplicationController
-  def index
-    @shops = Shop.all
-  end
-
   def show
     @shop = Shop.find_by_hostname(request.host) or render_404
   end
--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,6 @@ Shopapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'shops#index', :constraints => { :domain => /shop-?app\.(heroku(app)?\.)?(com|dev|se)/ }
   root :to => "shops#show"
 
   # See how all your routes lay out with "rake routes"
