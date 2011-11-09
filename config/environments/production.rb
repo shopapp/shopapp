4e3030fc8628a3fdf82181ad3269abfe707eb066
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 22:27:45 +0100

    Added public/assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Shopapp::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += ['active_admin.js', 'active_admin.css']
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
