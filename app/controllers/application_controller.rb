3c2f9669f52f32e77da3468ebc9fc4d4bf572c82
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 20:42:22 +0100

    Add shop-app.herokuapp.com to admin hosts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :shopapp?
+
+  def shopapp?
+    request.domain =~ /(shopapp|shop-app\.herokuapp)\.\w{2,3}/ 
+  end
 end
