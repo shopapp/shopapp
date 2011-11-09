be02cd712a8b2e4048a640f9f7e20831438b2409
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-11-09 22:46:56 +0100

    Improve shop index regexp

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Shopapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'shops#index', :constraints => { :domain => /(shopapp|shop-app\.herokuapp)\.\w{2,3}/ }
+  root :to => 'shops#index', :constraints => { :domain => /shop-?app\.(heroku(app)?\.)?(com|dev|se)/ }
   root :to => "shops#show"
 
   # See how all your routes lay out with "rake routes"
